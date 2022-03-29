197530e1353df10636f377939a7a26d8fa2eaf8f
Daniel Marchena Parreira <danielmapar@gmail.com>
2022-03-29 02:55:49 -0400

    Smallest Subarray with a Greater Sum (easy)

--- courses/grokking-the-coding-interview/sliding-window/solution3.java
+++ courses/grokking-the-coding-interview/sliding-window/solution3.java
@@ -1,54 +1,43 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class getSmallestSubArrayWithAGreaterSum {
-
-    public static Integer[] getSmallestSubArray(int[] arr, int S) {
-        List<Integer> currentSum = new ArrayList<>();
-        List<Integer> smallestSum = new ArrayList<>();
-        Integer sum = 0;
-
-        for (int val : arr) {
-            if (val > S && currentSum.size() > 0) {
-                sum -= currentSum.remove(0);
-                if (sum == S && (smallestSum.size() == 0 || smallestSum.size() > currentSum.size()))
-                    smallestSum = new ArrayList<>(currentSum);
-                sum = 0;
-                currentSum = new ArrayList<>();
-            }
-            else if (val == S) return new Integer[] { val };
-            else if (val < S) {
-                if ((val + sum) > S)
-                    sum -= currentSum.remove(0);
-
-                if (sum == S) {
-                    if (smallestSum.size() == 0 || smallestSum.size() > currentSum.size()) 
-                        smallestSum = new ArrayList<>(currentSum);
-                    currentSum = new ArrayList<>();
-                    sum = 0;
-                } else if ((val + sum) <= S) {
-                    currentSum.add(val);
-                    sum += val;
-                    if (sum == S) {
-                        if (smallestSum.size() == 0 || smallestSum.size() > currentSum.size()) 
-                            smallestSum = new ArrayList<>(currentSum);
-                        currentSum = new ArrayList<>();
-                        sum = 0;
-                    } 
-                } else {
-                    currentSum = new ArrayList<>();
-                    currentSum.add(val);
-                    sum = val;
-                }
+class SmallestSubArrayWithAGreaterSum {
+    public static int findMinSubArray(int[] arr, int S) {
+        int windowSum = 0, minLength = Integer.MAX_VALUE;
+        int windowStart = 0;
+        for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+          windowSum += arr[windowEnd]; // add the next element
+          // shrink the window as small as possible until the 'windowSum' is smaller than 'S'
+          while (windowSum >= S) {
+            minLength = Math.min(minLength, windowEnd - windowStart + 1);
+            windowSum -= arr[windowStart]; // subtract the element going out
+            windowStart++; // slide the window ahead
+          }
+        }
+    
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+      }
+    public static int getSmallestSubArraySize(int[] arr, int S) {
+        int sum = 0;
+        int max = Integer.MAX_VALUE;
+        int firstIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= S) return 1;
+            else if ((sum + arr[i]) < S) sum += arr[i];
+            else {
+                sum += arr[i];
+                max = Math.min(i - firstIndex + 1, max);
+                sum -= arr[firstIndex];
+                firstIndex++;
+                if (sum >= S) 
+                    max = Math.min(i - firstIndex + 1, max);
             }
         }
-        return smallestSum.toArray(new Integer[smallestSum.size()]);
+
+        return max;
     }
-
     public static void main(String[] args) {
-        Integer[] result = getSmallestSubArray(new int[] { 5, -1, 2, 4, 5, 5 }, 3);
-        for (Integer val : result) {
-            System.out.println(val);
-        }
+        System.out.println(getSmallestSubArraySize(new int[] {2, 1, 5, 2, 3, 2}, 7));
+        System.out.println(getSmallestSubArraySize(new int[] {2, 1, 5, 2, 8}, 7));
+        System.out.println(getSmallestSubArraySize(new int[] {3, 4, 1, 1, 6}, 8));
+
+        System.out.println(getSmallestSubArraySize(new int[] {3, 4, 11, 1, 36, 12, 3, 4, 5, 1, 123, 23, 1}, 8) == findMinSubArray(new int[] {3, 4, 11, 1, 36, 12, 3, 4, 5, 1, 123, 23, 1}, 8));
     }
 }
